88d9b3358c46b637122bb2735fdc928261dab525
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-02-23 18:15:04 +0900

    API を修正

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,22 +1,34 @@
 import { Injectable } from '@nestjs/common';
-import { PostMetaData } from '@nx-cms/model';
+import { PostMetaData, PostDetail } from '@nx-cms/model';
 
 @Injectable()
 export class AppService {
-  posts: PostMetaData[] = [
+  posts: PostDetail[] = [
     {
       id: 1,
       title: 'テスト投稿1',
-      published: new Date('2020-02-23T08:55:28.087Z')
+      published: new Date('2020-02-23T08:55:28.087Z'),
+      tags: ['Nx', 'Angular'],
+      article: ['皆さんこんにちは。', 'これはテスト投稿です。']
     },
     {
       id: 2,
       title: 'テスト投稿2',
-      published: new Date('2020-02-23T08:55:28.087Z')
+      published: new Date('2020-02-23T08:55:28.087Z'),
+      tags: ['Nx', 'Angular'],
+      article: ['皆さんこんにちは。', 'これはテスト投稿です。']
     }
   ];
 
   getPosts(): PostMetaData[] {
+    const postMetaDatas: PostMetaData[] = [];
+    this.posts.forEach((post: PostDetail) => {
+      postMetaDatas.push({
+        id: post.id,
+        title: post.title,
+        published: post.published
+      });
+    });
     return this.posts;
   }
 }
