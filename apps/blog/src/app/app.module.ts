6efbd62698372df5d34214a3b7de4a9fccf5a665
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-02-23 16:07:14 +0900

    ng g component pages/posts --project blog

--- apps/blog/src/app/app.module.ts
+++ apps/blog/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UiModule } from '@nx-cms/ui';
+import { PostsComponent } from './pages/posts/posts.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PostsComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
