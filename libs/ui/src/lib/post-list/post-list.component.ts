c96d783ed546df0fe0b030f6089e9ae4556b22c9
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-02-23 16:25:23 +0900

    ng g component post-list --project ui

---
+++ libs/ui/src/lib/post-list/post-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nx-cms-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   imports: [CommonModule, BrowserAnimationsModule, MatToolbarModule],
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, PostListComponent],
   exports: [HeaderComponent]
 })
 export class UiModule {}
