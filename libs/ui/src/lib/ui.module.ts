92896727dfe935197a4f7dc72a28700b24b003c3
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-02-23 14:47:08 +0900

    header component を生成

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  imports: [CommonModule, BrowserAnimationsModule]
+  imports: [CommonModule, BrowserAnimationsModule],
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent]
 })
 export class UiModule {}
